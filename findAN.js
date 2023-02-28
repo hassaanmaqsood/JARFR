5c5004cf07399acc11a8ad5c09bec905068961a9
Hassaan Maqsood <50172619+hassaanmaqsood@users.noreply.github.com>
2023-03-01 00:37:13 +0500

    Update findAN.js

--- findAN.js
+++ findAN.js
@@ -1,22 +1,25 @@
 function findAN(string) {
-  packetA=[];
-  packetN=[];
-  startIndex=0;
-  for (var i = 0; i < string.length; i++) {
-    if ("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".includes(string[i])) {
-      if (!"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".includes(string[i-1])) {
-        startIndex=i;
-      } else if (!"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".includes(string[i+1])) {
-        packetA.push(string.substring(startIndex, i+1));
+  const packetA = [];
+  const packetN = [];
+  let startIndex = 0;
+
+  for (let i = 0; i < string.length; i++) {
+    const char = string[i];
+
+    if (/[a-zA-Z]/.test(char)) {
+      if (!/[a-zA-Z]/.test(string[i - 1])) {
+        startIndex = i;
+      } else if (!/[a-zA-Z]/.test(string[i + 1])) {
+        packetA.push(string.substring(startIndex, i + 1));
       }
-    };
-    if ("0123456789".includes(string[i])) {
-      if (!"0123456789".includes(string[i-1])) {
-        startIndex=i;
-      } else if (!"0123456789".includes(string[i+1])) {
-        packetN.push(string.substring(startIndex, i+1));
+    } else if (/[0-9]/.test(char)) {
+      if (!/[0-9]/.test(string[i - 1])) {
+        startIndex = i;
+      } else if (!/[0-9]/.test(string[i + 1])) {
+        packetN.push(string.substring(startIndex, i + 1));
       }
-    };
-  };
+    }
+  }
+
   return [packetA, packetN];
-};
+}
